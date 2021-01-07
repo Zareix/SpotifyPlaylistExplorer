98ac5a7e1470eb716b84c7cdad01bd8333a36cc0
Zareix <raphcatarino@gmail.com>
2021-01-07 10:55:25 +0100

    list artists

--- src/BtnMoreInfoTrack.js
+++ src/BtnMoreInfoTrack.js
@@ -4,7 +4,7 @@ import Button from "react-bootstrap/Button";
 import Modal from "react-modal";
 
 const BtnMoreInfoTrack = (props) => {
-  Modal.setAppElement(document.getElementById('App'));
+  Modal.setAppElement(document.getElementById("App"));
 
   const customStyles = {
     content: {
@@ -36,19 +36,49 @@ const BtnMoreInfoTrack = (props) => {
         style={customStyles}
         contentLabel="Example Modal"
       >
+        <div className="text-center">
           {props.track.album.images[0] && (
-              <img src={props.track.album.images[0].url} width={300} className="mb-2" alt={`album-img-${props.track.album.name}`}/>
+            <img
+              src={props.track.album.images[0].url}
+              width={300}
+              className="mb-2"
+              alt={`album-img-${props.track.album.name}`}
+            />
           )}
-        <h1>{props.track.name}</h1>
-        <h3> - {props.track.artists[0].name}</h3>
-        <div className="mt-3 mb-3">
-            <h5 className="mb-0">Album :<br></br></h5>
-            <span className="ms-3">Type :  {props.track.album.album_type}<br></br></span>
-            <span className="ms-3">Nom : {props.track.album.name}<br></br></span>
-            <span className="ms-3">Date de sortie : {props.track.album.release_date}<br></br></span>
+          <h1>{props.track.name}</h1>
+          <h3>
+            {props.track.artists.map((artist) => {
+              return (
+                <span>
+                  {props.track.artists.length - 1 ===
+                  props.track.artists.indexOf(artist) ? (
+                    <span>{artist.name} </span>
+                  ) : (
+                    <span>{artist.name}, </span>
+                  )}
+                </span>
+              );
+            })}
+          </h3>
+          <div className="mt-3 mb-3">
+            <h5 className="mb-0">
+              Album :<br></br>
+            </h5>
+            <span className="ms-3">
+              Type : {props.track.album.album_type}
+              <br></br>
+            </span>
+            <span className="ms-3">
+              Nom : {props.track.album.name}
+              <br></br>
+            </span>
+            <span className="ms-3">
+              Date de sortie : {props.track.album.release_date}
+              <br></br>
+            </span>
+          </div>
+          <a href={props.track.external_urls.spotify}>Lien sur Spotify</a>
         </div>
-        <a href={props.track.external_urls.spotify}>Lien sur Spotify</a>
-        
       </Modal>
     </div>
   );
